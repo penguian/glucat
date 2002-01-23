0e0d386102457fc3c33001fcee002a5672ff6c78
Paul C. Leopardi <leopardi@users.sourceforge.net>
2002-01-23 21:42:27 +0000

    Take account of irredicible representations by testing Cl(n,0)

--- test/squaring.h
+++ test/squaring.h
@@ -83,6 +83,9 @@ namespace glucat_mult_test
     static const index_t v_hi = e_::v_hi;
 
     m_ a;
+    const index_t max_pos = min(index_t(2*n), max_n);
+    const e_ pos_frame = index_range<v_lo,v_hi>(1, max_pos);
+
     const index_t max_index = min(n, max_n);
     const e_ outer_frame = index_range<v_lo,v_hi>(-max_index, max_index);
     clock_t cpu_time = clock();
@@ -94,12 +97,12 @@ namespace glucat_mult_test
     const double mult_cpu_time = elapsed(cpu_time);
     print_times(outer_frame, outer_frame, setup_cpu_time, mult_cpu_time);
     cout << "Square is " << a << endl;
-    a = m_(1, outer_frame);
+    a = m_(1, pos_frame);
     e_ inner_frame;
-    for (index_t i = 1; i != max_index+1; i++)
+    for (index_t i = 1; i != max_pos+1; i++)
     {
       inner_frame |= e_(i);
-      time_mult(a, m_(e_(i) , 1.0, outer_frame), i, 0,   inner_frame, outer_frame);
+      time_mult(a, m_(e_(i) , 1.0, pos_frame), i, 0,   inner_frame, pos_frame);
     }
     inner_frame = e_();
     a = m_(1, outer_frame);
