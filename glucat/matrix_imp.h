0c6b4742e6d171a10daceb0dad1a92701a489709
Paul C. Leopardi <leopardi@users.sourceforge.net>
2002-12-28 23:15:47 +0000

    Use uBLAS rather than MTL. Use namespace matrix to reduce clutter in namespace glucat. Remove is_perm_shaped() and is_singlet() as they are no longer needed. Add operator==, mono_prod() and compressed_prod() for matrix expressions. Change interface so that kron and unit return Matrix_T rather than void.

--- glucat/matrix_imp.h
+++ glucat/matrix_imp.h
@@ -23,76 +23,109 @@
      See also Arvind Raja's original header comments in glucat.h
  ***************************************************************************/
 
-namespace glucat
+namespace glucat { namespace matrix
 {
   /// Kronecker tensor product of matrices - as per Matlab kron
-  template< class Matrix_T >
-  void
-  kron(const Matrix_T& x, const Matrix_T& y, Matrix_T& z)
+  template< typename Matrix_T >
+  const
+  Matrix_T
+  kron(const Matrix_T& lhs, const Matrix_T& rhs)
   {
-    const int rx = x.nrows();
-    const int cx = x.ncols();
-    const int ry = y.nrows();
-    const int cy = y.ncols();
-    z = Matrix_T(rx*ry, cx*cy);
-    mtl::set(z, 0);
-    for (      typename Matrix_T::const_iterator i = x.begin(); i != x.end(); ++i)
-      for (    typename Matrix_T::Row::const_iterator j = (*i).begin(); j != (*i).end(); ++j)
-        for (  typename Matrix_T::const_iterator k = y.begin(); k != y.end(); ++k)
-          for (typename Matrix_T::Row::const_iterator l = (*k).begin(); l != (*k).end(); ++l)
-              z(j.row()*ry + l.row(), j.column()*cy + l.column()) = (*j) * (*l);
+    typedef typename Matrix_T::size_type  matrix_index_t;
+    typedef typename Matrix_T::value_type scalar_t;
+    const matrix_index_t lhs_nrows = lhs.size1();
+    const matrix_index_t lhs_ncols = lhs.size2();
+    const matrix_index_t rhs_nrows = rhs.size1();
+    const matrix_index_t rhs_ncols = rhs.size2();
+    Matrix_T result(lhs_nrows*rhs_nrows, lhs_ncols*rhs_ncols);
+
+    for (      typename Matrix_T::const_iterator1 i = lhs.begin1(); i != lhs.end1(); ++i)
+      for (    typename Matrix_T::const_iterator2 j = i.begin(); j != i.end(); ++j)
+      {
+        const matrix_index_t rj1 = j.index1()*rhs_nrows;
+        const matrix_index_t cj2 = j.index2()*rhs_ncols;
+        const scalar_t lhs_ij = (*j);
+        for (  typename Matrix_T::const_iterator1 k = rhs.begin1(); k != rhs.end1(); ++k)
+          for (typename Matrix_T::const_iterator2 l = k.begin(); l != k.end(); ++l)
+              result(rj1 + l.index1(), cj2 + l.index2()) = lhs_ij * (*l);
+      }
+    return result;
   }
 
   /// Unit matrix - as per Matlab eye
-  template< class Matrix_T >
-  void
-  unit(int n, Matrix_T& result)
+  template< typename Matrix_T >
+  const
+  Matrix_T
+  unit(const typename Matrix_T::size_type dim)
   {
-    mtl::set(result, 0);
-    mtl::set_diagonal(result, 1);
+    typedef typename Matrix_T::size_type matrix_index_t;
+    Matrix_T result(dim, dim);
+    for (matrix_index_t k = 0; k != dim; ++k)
+      result(k, k) = 1;
+    return result;
   }
 
-  /// Does a matrix have only one non-zero per row (or column) ?
-  // Note: This will return false for dense matrices,
-  // but this is OK for the current use of this function,
-  // which is to help determine when a compressed multiply should have a dense result
-  template< class Matrix_T >
+  /// Equality of matrices
+  template< typename LHS_T, typename RHS_T >
+  inline
   bool
-  is_singlet(const Matrix_T& x)
-  {
-    for (typename Matrix_T::const_iterator i = x.begin(); i != x.end(); ++i)
-      if ((*i).size() != 1)
-        return false;
-    return true;
-  }
+  operator==(const ublas::matrix_expression<LHS_T>& lhs,
+             const ublas::matrix_expression<RHS_T>& rhs)
+  { return ublas::equals(lhs, rhs); }
 
-  /// Does a matrix have only one non-zero per row and column ?
-  // Note: This will return false for dense matrices,
-  // but this is OK for the current use of this function,
-  // which is to help determine when a compressed multiply should have a dense result
-  template< class Matrix_T >
-  bool
-  is_perm_shaped(const Matrix_T& x)
+  /// Product of monomial matrices
+  template< typename Matrix_T, typename LHS_T, typename RHS_T >
+  const Matrix_T
+  mono_prod(const ublas::matrix_expression<LHS_T>& lhs,
+            const ublas::matrix_expression<RHS_T>& rhs)
   {
-    if (is_singlet(x))
+    typedef const LHS_T lhs_expression_t;
+    typedef const RHS_T rhs_expression_t;
+    typedef typename Matrix_T::size_type  matrix_index_t;
+    typedef typename Matrix_T::value_type scalar_t;
+    typedef typename lhs_expression_t::const_iterator1   lhs_const_iterator1;
+    typedef typename lhs_expression_t::const_iterator2   lhs_const_iterator2;
+    typedef typename ublas::matrix_row<rhs_expression_t> matrix_row_t;
+    typedef typename matrix_row_t::const_iterator        row_const_iterator;
+
+    const matrix_index_t dim = lhs().size1();
+    Matrix_T result(dim, dim);
+    for (lhs_const_iterator1 lhs_row = lhs().begin1(); lhs_row != lhs().end1(); ++lhs_row)
     {
-      Matrix_T xt(x.ncols(), x.nrows());
-      mtl::transpose(x, xt);
-      return is_singlet(xt);
+      const lhs_const_iterator2& lhs_it = lhs_row.begin();
+      if (lhs_it != lhs_row.end())
+      {
+        const matrix_row_t rhs_row(rhs(), lhs_it.index2());
+        const row_const_iterator&  rhs_it = rhs_row.begin();
+        if (rhs_it != rhs_row.end())
+          result(lhs_it.index1(), rhs_it.index()) = (*lhs_it) * (*rhs_it);
+      }
     }
-    return false;
+    return result;
   }
 
-  /// Inner product: sum(x(i,j)*y(i,j))/x.nrows()
-  template< class Matrix_T, class Scalar_T >
+  /// Product of compressed matrices
+  template< typename Matrix_T, typename LHS_T, typename RHS_T >
+  inline
+  const Matrix_T
+  compressed_prod(const ublas::matrix_expression<LHS_T>& lhs,
+                  const ublas::matrix_expression<RHS_T>& rhs)
+  { return ublas::sparse_prod<Matrix_T>(lhs, rhs, typename Matrix_T::orientation_category()); }
+
+  /// Inner product: sum(lhs(i,j)*rhs(i,j))/lhs.nrows()
+  template< typename Matrix_T, typename Scalar_T >
   Scalar_T
-  inner(const Matrix_T& x, const Matrix_T& y)
+  inner(const Matrix_T& lhs, const Matrix_T& rhs)
   {
     Scalar_T result = 0;
-    for (typename Matrix_T::const_iterator i = x.begin(); i != x.end(); ++i)
-      for (typename Matrix_T::Row::const_iterator j = (*i).begin(); j != (*i).end(); ++j)
-        result += (*j) * y(j.row(),j.column());
-    return result / x.nrows();
+    for (typename Matrix_T::const_iterator1 i = lhs.begin1(); i != lhs.end1(); ++i)
+      for (typename Matrix_T::const_iterator2 j = i.begin(); j != i.end(); ++j)
+      {
+        const Scalar_T rhs_j12 = rhs(j.index1(),j.index2());
+        if (rhs_j12 != Scalar_T(0))
+          result += (*j) * rhs_j12;
+      }
+    return result / lhs.size1();
   }
-}
+} }
 #endif  // _GLUCAT_MATRIX_IMP_H
