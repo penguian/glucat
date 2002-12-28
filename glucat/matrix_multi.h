74b93c5b88dbd791b354db48259cea0179897429
Paul C. Leopardi <leopardi@users.sourceforge.net>
2002-12-28 23:48:58 +0000

    Prefer std::string to char* for classname. Use uBLAS rather than MTL. Use _GLUCAT_PUBLIC for Intel C++ compatibility. Remove copy constructor - no longer needed. Have basis_element() return matrix_t rather than void. Minor formatting changes.

--- glucat/matrix_multi.h
+++ glucat/matrix_multi.h
@@ -34,77 +34,72 @@ namespace glucat
   public clifford_algebra< Scalar_T, index_set<LO,HI>, matrix_multi<Scalar_T,LO,HI> >
   {
   public:
-    typedef matrix_multi                  multivector_t;
-    typedef Scalar_T                      scalar_t;
-    typedef index_set<LO,HI>              index_set_t;
-    typedef std::pair< const index_set_t, Scalar_T >  pair_t;
-    typedef std::vector<Scalar_T>         vector_t;
-    typedef error<multivector_t>          error_t;
-    typedef multivector_t                 matrix_multi_t;
-    typedef framed_multi<Scalar_T,LO,HI>  framed_multi_t;
+    typedef matrix_multi                             multivector_t;
+    typedef multivector_t                            matrix_multi_t;
+    typedef Scalar_T                                 scalar_t;
+    typedef index_set<LO,HI>                         index_set_t;
+    typedef std::pair< const index_set_t, Scalar_T > pair_t;
+    typedef std::vector<Scalar_T>                    vector_t;
+    typedef error<multivector_t>                     error_t;
+    typedef framed_multi<Scalar_T,LO,HI>             framed_multi_t;
     friend class framed_multi_t;
-  private:
-    typedef typename mtl::matrix< Scalar_T, mtl::rectangle<>, mtl::compressed<>,
-            mtl::row_major >::type        sparse_matrix_t;
-    typedef typename mtl::matrix< Scalar_T, mtl::rectangle<>, mtl::dense<>,
-            mtl::row_major >::type        dense_matrix_t;
-    typedef sparse_matrix_t               matrix_t;
-    typedef typename mtl::matrix_traits<matrix_t>::size_type
-                                          matrix_index_t;
+  _GLUCAT_PRIVATE:
+    typedef ublas::row_major                         orientation_t;
+    typedef ublas::compressed_matrix< Scalar_T, orientation_t >
+                                                     sparse_matrix_t;
+    typedef ublas::compressed_matrix< Scalar_T, ublas::column_major >
+                                                     col_matrix_t;
+    typedef ublas::matrix< Scalar_T, orientation_t > dense_matrix_t;
+    typedef sparse_matrix_t                          matrix_t;
+    typedef typename matrix_t::size_type             matrix_index_t;
   public:
     /// Class name used in messages
-    static const char* classname();
-		/// Destructor
-		~matrix_multi() {};
+    static const std::string classname();
+    /// Destructor
+    ~matrix_multi() {};
     /// Default constructor
     matrix_multi();
-    /// Copy constructor
-    matrix_multi(const multivector_t& val);
-  	/// Construct a multivector, within a given frame, from a given multivector
-    matrix_multi(const multivector_t& val,
-								 const index_set_t& frm, const bool prechecked = false);
+    /// Construct a multivector, within a given frame, from a given multivector
+    matrix_multi(const multivector_t& val, 
+                 const index_set_t& frm, const bool prechecked = false);
     /// Construct a multivector from an index set and a scalar coordinate
     matrix_multi(const index_set_t& ist, const Scalar_T& crd);
-  	/// Construct a multivector, within a given frame, from an index set and a scalar coordinate
+    /// Construct a multivector, within a given frame, from an index set and a scalar coordinate
     matrix_multi(const index_set_t& ist, const Scalar_T& crd,
-								 const index_set_t& frm, const bool prechecked = false);
+                 const index_set_t& frm, const bool prechecked = false);
     /// Construct a multivector from a scalar (within a frame, if given)
     matrix_multi(const Scalar_T& scr, const index_set_t& frm = index_set_t());
     /// Construct a multivector from an int (within a frame, if given)
     matrix_multi(const int scr, const index_set_t& frm = index_set_t());
-  	/// Construct a multivector, within a given frame, from a given vector
+    /// Construct a multivector, within a given frame, from a given vector
     matrix_multi(const vector_t& vec,
-								 const index_set_t& frm, const bool prechecked = false);
+                 const index_set_t& frm, const bool prechecked = false);
     /// Construct a multivector from a string: eg: "3+2{1,2}-6.1e-2{2,3}"
     matrix_multi(const std::string& str);
     /// Construct a multivector, within a given frame, from a string: eg: "3+2{1,2}-6.1e-2{2,3}"
     matrix_multi(const std::string& str,
-								 const index_set_t& frm, const bool prechecked = false);
-  	/// Construct a multivector from a framed_multi_t
+                 const index_set_t& frm, const bool prechecked = false);
+    /// Construct a multivector from a framed_multi_t
     matrix_multi(const framed_multi_t& val);
-  	/// Construct a multivector, within a given frame, from a framed_multi_t
+    /// Construct a multivector, within a given frame, from a framed_multi_t
     matrix_multi(const framed_multi_t& val,
-								 const index_set_t& frm, const bool prechecked = false);
+                 const index_set_t& frm, const bool prechecked = false);
   private:
     /// Construct a multivector within a given frame from a given matrix
     matrix_multi(const matrix_t& mtx, const index_set_t& frm);
   public:
     _GLUCAT_CLIFFORD_ALGEBRA_OPERATIONS
-		/// Assignment operator
-    multivector_t&           operator= (const multivector_t& rhs);
+    /// Assignment operator
+    multivector_t&     operator= (const multivector_t& rhs);
   private:
     /// Add a term, if non-zero
     multivector_t&     operator+= (const pair_t& rhs);
-#ifdef __ICL
-  public: // hack for icc 6
-#else
-  private:
-#endif
+  _GLUCAT_PRIVATE:
     // Data members
     /// Index set representing the frame for the subalgebra which contains the multivector
-    index_set_t     m_frame;
+    index_set_t        m_frame;
     /// Matrix value representing the multivector within the folded frame
-    matrix_t        m_matrix;
+    matrix_t           m_matrix;
   };
   // non-members
 
@@ -120,8 +115,8 @@ namespace glucat
 
   /// Create a basis element matrix within a frame
   template< typename Scalar_T, const index_t LO, const index_t HI >
-  void
-  basis_element( const index_set<LO,HI>& ist, const index_set<LO,HI>& sub,
-                 typename matrix_multi<Scalar_T,LO,HI>::matrix_t& result );
+  const 
+  typename matrix_multi<Scalar_T,LO,HI>::matrix_t
+  basis_element(const index_set<LO,HI>& ist, const index_set<LO,HI>& sub);
 }
 #endif  // _GLUCAT_MATRIX_MULTI_H
