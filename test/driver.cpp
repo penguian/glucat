df679606f3c2299835b2a9585913db788e2680dd
Paul C. Leopardi <leopardi@users.sourceforge.net>
2003-03-01 14:40:25 +0000

    Cosmetic change.

--- test/driver.cpp
+++ test/driver.cpp
@@ -32,7 +32,7 @@ int main(int argc, char ** argv)
   {
     test1, test2, test3, test4, test5, test6, test7, test8,
     test9, test10,test11,test12,test13,test14,test15,test16
-  } ;
+  };
   const long int NBRTESTS = sizeof(testfn)/sizeof(intfn *);
   bitset<NBRTESTS> tests;
   if (argc == 1)
