b727ec2c69cc93929e5d4ce3e47561347249d59b
Paul C. Leopardi <leopardi@users.sourceforge.net>
2003-06-20 14:08:08 +0000

    Workaround change to gcc 3.3 which applies visibility rules more strictly, by defining _GLUCAT_PRIVATE to be public in this case.

--- glucat/portability.h
+++ glucat/portability.h
@@ -28,8 +28,8 @@
 #define ios_base ios // scope is different for standard C++
 #endif
 
-//***************************** workaround for ICC
-#if defined (__ICL) || defined (__ICC)
+//***************************** workaround for ICC and G++ 3.3+
+#if defined (__ICL) || defined (__ICC) || defined (__GNUG__) && (__GNUC__ >= 3) && (__GNUC_MINOR__ >= 3)
 #define _GLUCAT_PRIVATE public
 #else
 #define _GLUCAT_PRIVATE private
