bb25a816890d9214b0000a23da1475a1f508417a
Paul C. Leopardi <leopardi@users.sourceforge.net>
2007-01-14 09:33:42 +0000

    Make nork faster: iterate over sparse rhs_range rather than scaling and adding whole range. Make kron and inner match naming conventions used by nork. Prefer const Scalar_T& to const Scalar_T, ie. use const references for constant scalars. Cosmetic: Prefer T a = T(..) form of non-default construction; the form T a(..) is sometimes confused with a function declaration. Cosmetic: Prefer Scalar_T to scalar_t. Cosmetic: Prefer Scalar_T(-1) to Scalar_T(-1.0) etc. Cosmetic: In nork, using "ublas::range" rather than "using namespace ublas". Update copyright notice. Drop old email address.

--- glucat/matrix_imp.h
+++ glucat/matrix_imp.h
@@ -5,9 +5,8 @@
     matrix_imp.h : Implement common matrix functions
                              -------------------
     begin                : Sun 2001-12-09
-    copyright            : (C) 2001 by Paul C. Leopardi
+    copyright            : (C) 2001-2007 by Paul C. Leopardi
                          : uBLAS interface contributed by Joerg Walter
-    email                : leopardi@bigpond.net.au
  ***************************************************************************
  *   This library is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Lesser General Public License as        *
@@ -32,35 +31,37 @@ namespace glucat { namespace matrix
   Matrix_T
   kron(const Matrix_T& lhs, const Matrix_T& rhs)
   {
-    typedef typename Matrix_T::size_type  matrix_index_t;
-    typedef typename Matrix_T::value_type scalar_t;
-    const matrix_index_t lhs_nrows = lhs.size1();
-    const matrix_index_t lhs_ncols = lhs.size2();
-    const matrix_index_t rhs_nrows = rhs.size1();
-    const matrix_index_t rhs_ncols = rhs.size2();
-    Matrix_T result(lhs_nrows*rhs_nrows, lhs_ncols*rhs_ncols);
-
-    for (typename Matrix_T::const_iterator1 
-        i = lhs.begin1(); 
-        i != lhs.end1(); 
-        ++i)
-      for (typename Matrix_T::const_iterator2 
-          j = i.begin(); 
-          j != i.end(); 
-          ++j)
+    typedef Matrix_T matrix_t;
+    typedef typename matrix_t::size_type matrix_index_t;
+    const matrix_index_t lhs_s1 = lhs.size1();
+    const matrix_index_t lhs_s2 = lhs.size2();
+    const matrix_index_t rhs_s1 = rhs.size1();
+    const matrix_index_t rhs_s2 = rhs.size2();
+    matrix_t result(lhs_s1*rhs_s1, lhs_s2*rhs_s2);
+    typedef typename matrix_t::value_type Scalar_T;
+    typedef typename matrix_t::const_iterator1 const_iterator1;
+    typedef typename matrix_t::const_iterator2 const_iterator2;
+    for (const_iterator1
+        lhs_it1 = lhs.begin1();
+        lhs_it1 != lhs.end1();
+        ++lhs_it1)
+      for (const_iterator2
+          lhs_it2 = lhs_it1.begin();
+          lhs_it2 != lhs_it1.end();
+          ++lhs_it2)
       {
-        const matrix_index_t rj1 = j.index1()*rhs_nrows;
-        const matrix_index_t cj2 = j.index2()*rhs_ncols;
-        const scalar_t lhs_ij = (*j);
-        for (typename Matrix_T::const_iterator1 
-            k = rhs.begin1(); 
-            k != rhs.end1(); 
-            ++k)
-          for (typename Matrix_T::const_iterator2 
-              l = k.begin(); 
-              l != k.end(); 
-              ++l)
-            result(rj1 + l.index1(), cj2 + l.index2()) = lhs_ij * (*l);
+        const matrix_index_t start1 = rhs_s1 * lhs_it2.index1();
+        const matrix_index_t start2 = rhs_s2 * lhs_it2.index2();
+        const Scalar_T& lhs_val = (*lhs_it2);
+        for (const_iterator1
+            rhs_it1 = rhs.begin1();
+            rhs_it1 != rhs.end1();
+            ++rhs_it1)
+          for (const_iterator2
+              rhs_it2 = rhs_it1.begin();
+              rhs_it2 != rhs_it1.end();
+              ++rhs_it2)
+            result(start1 + rhs_it2.index1(), start2 + rhs_it2.index2()) = lhs_val * *rhs_it2;
       }
     return result;
   }
@@ -89,31 +90,38 @@ namespace glucat { namespace matrix
       throw error_t("matrix", "nork: incompatible numbers of rows");
     if (res_s2 * lhs_s2 != rhs_s2)
       throw error_t("matrix", "nork: incompatible numbers of cols");
-    typedef typename matrix_t::value_type scalar_t;
-    const scalar_t nnz_lhs = scalar_t( mono ? lhs_s1 : nnz(lhs) );
-    if (nnz_lhs == scalar_t(0))
+    typedef typename matrix_t::value_type Scalar_T;
+    const Scalar_T& nnz_lhs = Scalar_T( mono ? lhs_s1 : nnz(lhs) );
+    if (nnz_lhs == Scalar_T(0))
       throw error_t("matrix", "nork: LHS must not be 0");
-    matrix_t result(res_s1, res_s2);
-    typedef typename matrix_t::const_iterator1 const_iterator1;
-    typedef typename matrix_t::const_iterator2 const_iterator2;
-    for (const_iterator1 
+    matrix_t result = matrix_t(res_s1, res_s2);
+    for (typename matrix_t::const_iterator1
         lhs_it1 = lhs.begin1();
-        lhs_it1 != lhs.end1(); 
+        lhs_it1 != lhs.end1();
         ++lhs_it1)
-      for (const_iterator2 
+      for (typename matrix_t::const_iterator2
           lhs_it2 = lhs_it1.begin();
-          lhs_it2 != lhs_it1.end(); 
+          lhs_it2 != lhs_it1.end();
           ++lhs_it2)
-        if (*lhs_it2 != scalar_t(0))
+        if (*lhs_it2 != Scalar_T(0))
         {
-          using namespace ublas;
-          typedef matrix_range<const matrix_t> matrix_range_t;
+          typedef ublas::matrix_range<const matrix_t> matrix_range_t;
+          const Scalar_T& lhs_scale = *lhs_it2 * nnz_lhs;
           const matrix_index_t start1 = res_s1 * lhs_it2.index1();
           const matrix_index_t start2 = res_s2 * lhs_it2.index2();
-          const range& range1 = range(start1, start1+res_s1);
-          const range& range2 = range(start2, start2+res_s2);
+          using ublas::range;
+          const range& range1 = range(start1, start1 + res_s1);
+          const range& range2 = range(start2, start2 + res_s2);
           const matrix_range_t& rhs_range = matrix_range_t(rhs, range1, range2);
-          result += rhs_range / (*lhs_it2 * nnz_lhs);
+          for (typename matrix_range_t::const_iterator1
+              rhs_it1 = rhs_range.begin1();
+              rhs_it1 != rhs_range.end1();
+              ++rhs_it1)
+            for (typename matrix_range_t::const_iterator2
+                rhs_it2 = rhs_it1.begin();
+                rhs_it2 != rhs_it1.end();
+                ++rhs_it2)
+                result(rhs_it2.index1(), rhs_it2.index2()) += *rhs_it2 / lhs_scale;
         }
     return result;
   }
@@ -129,13 +137,13 @@ namespace glucat { namespace matrix
     typedef typename matrix_t::const_iterator1 const_iterator1;
     typedef typename matrix_t::const_iterator2 const_iterator2;
     matrix_index_t result = 0;
-    for (const_iterator1 
-        it1 = m.begin1(); 
-        it1 != m.end1(); 
+    for (const_iterator1
+        it1 = m.begin1();
+        it1 != m.end1();
         ++it1)
-      for (const_iterator2 
-          it2 = it1.begin(); 
-          it2 != it1.end(); 
+      for (const_iterator2
+          it2 = it1.begin();
+          it2 != it1.end();
           ++it2)
         if (*it2 != 0)
           ++result;
@@ -149,8 +157,8 @@ namespace glucat { namespace matrix
   Matrix_T
   unit(const typename Matrix_T::size_type dim)
   {
-    typedef typename Matrix_T::value_type scalar_t;
-    return ublas::identity_matrix<scalar_t>(dim);
+    typedef typename Matrix_T::value_type Scalar_T;
+    return ublas::identity_matrix<Scalar_T>(dim);
   }
 
   /// Product of monomial matrices
@@ -163,24 +171,24 @@ namespace glucat { namespace matrix
     typedef const RHS_T rhs_expression_t;
     typedef typename RHS_T::expression_type matrix_t;
     typedef typename matrix_t::size_type  matrix_index_t;
-    typedef typename matrix_t::value_type scalar_t;
+    typedef typename matrix_t::value_type Scalar_T;
     typedef typename lhs_expression_t::const_iterator1   lhs_const_iterator1;
     typedef typename lhs_expression_t::const_iterator2   lhs_const_iterator2;
     typedef typename ublas::matrix_row<rhs_expression_t> matrix_row_t;
     typedef typename matrix_row_t::const_iterator        row_const_iterator;
 
     const matrix_index_t dim = lhs().size1();
-    matrix_t result(dim, dim);
-    for (lhs_const_iterator1 
-        lhs_row = lhs().begin1(); 
-        lhs_row != lhs().end1(); 
+    matrix_t result = matrix_t(dim, dim);
+    for (lhs_const_iterator1
+        lhs_row = lhs().begin1();
+        lhs_row != lhs().end1();
         ++lhs_row)
     {
       const lhs_const_iterator2& lhs_it = lhs_row.begin();
       if (lhs_it != lhs_row.end())
       {
-        const matrix_row_t rhs_row(rhs(), lhs_it.index2());
-        const row_const_iterator&  rhs_it = rhs_row.begin();
+        const matrix_row_t& rhs_row = matrix_row_t(rhs(), lhs_it.index2());
+        const row_const_iterator& rhs_it = rhs_row.begin();
         if (rhs_it != rhs_row.end())
           result(lhs_it.index1(), rhs_it.index()) = (*lhs_it) * (*rhs_it);
       }
@@ -195,8 +203,8 @@ namespace glucat { namespace matrix
   sparse_prod(const ublas::matrix_expression<LHS_T>& lhs,
               const ublas::matrix_expression<RHS_T>& rhs)
   {
-    typedef typename RHS_T::expression_type matrix_t;
-    return ublas::sparse_prod<matrix_t>(lhs(), rhs());
+    typedef typename RHS_T::expression_type expression_t;
+    return ublas::sparse_prod<expression_t>(lhs(), rhs());
   }
 
   /// Inner product: sum(lhs(i,j)*rhs(i,j))/lhs.nrows()
@@ -204,19 +212,19 @@ namespace glucat { namespace matrix
   Scalar_T
   inner(const Matrix_T& lhs, const Matrix_T& rhs)
   {
-    Scalar_T result = 0;
-    for (typename Matrix_T::const_iterator1 
-        i = lhs.begin1(); 
-        i != lhs.end1(); 
-        ++i)
-      for (typename Matrix_T::const_iterator2 
-          j = i.begin(); 
-          j != i.end(); 
-          ++j)
+    Scalar_T result = Scalar_T(0);
+    for (typename Matrix_T::const_iterator1
+        lhs_it1 = lhs.begin1();
+        lhs_it1 != lhs.end1();
+        ++lhs_it1)
+      for (typename Matrix_T::const_iterator2
+          lhs_it2 = lhs_it1.begin();
+          lhs_it2 != lhs_it1.end();
+          ++lhs_it2)
       {
-        const Scalar_T rhs_j12 = rhs(j.index1(),j.index2());
-        if (rhs_j12 != Scalar_T(0))
-          result += (*j) * rhs_j12;
+        const Scalar_T& rhs_val = rhs(lhs_it2.index1(),lhs_it2.index2());
+        if (rhs_val != Scalar_T(0))
+          result += (*lhs_it2) * rhs_val;
       }
     return result / lhs.size1();
   }
