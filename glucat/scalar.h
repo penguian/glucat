6b42532d1f0047b84ca832953b3d6870cd30153b
Paul C. Leopardi <leopardi@users.sourceforge.net>
2007-08-22 11:58:29 +0000

    Relicense under LGPLv3. Move definition of numeric_traits<> here. Define smart isInf(), smart isNaN_or_isInf() and NaN().

--- glucat/scalar.h
+++ glucat/scalar.h
@@ -7,11 +7,20 @@
     begin                : 2001-12-20
     copyright            : (C) 2001-2007 by Paul C. Leopardi
  ***************************************************************************
- *   This library is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Lesser General Public License as        *
- *   published by the Free Software Foundation; either version 2.1 of the  *
- *   License, or (at your option) any later version.                       *
- *   See http://www.fsf.org/copyleft/lesser.html for details               *
+
+    This library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
  ***************************************************************************
  This library is based on a prototype written by Arvind Raja and was
  licensed under the LGPL with permission of the author. See Arvind Raja,
@@ -30,5 +39,77 @@ namespace glucat
   Scalar_T
   log2(const Scalar_T x)
   { return std::log(x)/Scalar_T(l_ln2); }
+
+  /// Extra traits which extend numeric limits
+  // Reference: [AA], 2.4, p. 30-31
+  template< typename T >
+  class numeric_traits
+  {
+  private:
+    /// Smart isinf specialised for T without infinity
+    inline
+    static 
+    bool
+    isInf(T val, bool_to_type<false>)
+    { return false; }
+    /// Smart isnan specialised for T with quiet NaN
+    inline
+    static 
+    bool
+    isInf(T val, bool_to_type<true>)
+    { return std::isinf(val); }
+    /// Smart isnan specialised for T without quiet NaN
+    inline
+    static 
+    bool
+    isNaN(T val, bool_to_type<false>)
+    { return false; }
+    /// Smart isnan specialised for T with quiet NaN
+    inline
+    static 
+    bool
+    isNaN(T val, bool_to_type<true>)
+    { return std::isnan(val); }
+
+  public:
+    /// Smart isinf
+    inline
+    static 
+    bool
+    isInf(T val)
+    {
+      return isInf(val,
+             bool_to_type< std::numeric_limits<T>::has_infinity >() );
+    }
+    /// Smart isnan
+    inline
+    static 
+    bool
+    isNaN(T val)
+    {
+      return isNaN(val,
+             bool_to_type< std::numeric_limits<T>::has_quiet_NaN >() );
+    }
+    /// Smart isnan or isinf
+    inline
+    static 
+    bool
+    isNaN_or_isInf(T val)
+    {
+      return isNaN(val,
+             bool_to_type< std::numeric_limits<T>::has_quiet_NaN >() )
+          || isInf(val,
+             bool_to_type< std::numeric_limits<T>::has_infinity >() );
+    }
+    /// Smart NaN
+    static const T
+    NaN()
+    {
+      return std::numeric_limits<T>::has_quiet_NaN 
+           ? std::numeric_limits<T>::quiet_NaN() 
+           : T(std::log(0.0));
+    }
+  };
+
 }
 #endif // _GLUCAT_SCALAR_H
