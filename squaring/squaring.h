37109735d53d998eb3febfccc458498ec62c7496
Paul C. Leopardi <leopardi@users.sourceforge.net>
2007-09-09 08:14:55 +0000

    Clean up based on products test. Increase precision of output and increase number of repetitions to get more accurate timings for small frame sizes. Use new index_set(range) constructor.

--- squaring/squaring.h
+++ squaring/squaring.h
@@ -42,10 +42,8 @@ namespace glucat_mult_test
 {
   using namespace glucat;
   using namespace std;
+
   const index_t max_n = DEFAULT_HI;
-  typedef double Scalar_T;
-  typedef index_set< -max_n, max_n > index_set_t;
-  const Scalar_T RAND_SCALE = 1.0/RAND_MAX;
 
   inline
   static
@@ -53,10 +51,12 @@ namespace glucat_mult_test
   elapsed( clock_t cpu_time )
   { return ((clock() - cpu_time)*MS_PER_S) / CLOCKS_PER_SEC; }
 
+  template < typename Index_Set_T >
   inline
   void
-  print_times(const index_set_t& frame1, const index_set_t& frame2,
-              const double setup_cpu_time, const double mult_cpu_time)
+  print_times(const Index_Set_T& frame1, const Index_Set_T& frame2,
+              const double setup_cpu_time, 
+              const double prod_cpu_time)
   {
     const int index_width = 2;
     cout << "Cl(" << setw(index_width) <<  max_pos(frame1) << ","
@@ -67,59 +67,68 @@ namespace glucat_mult_test
     const ios::fmtflags& old_flags = cout.flags();
     const streamsize width = 12;
     const streamsize old_prec = cout.precision();
-    const streamsize new_prec = 2;
+    const streamsize new_prec = 3;
     cout.setf(ios_base::fixed);
     cout.setf(ios_base::showpoint);
     cout << setprecision(new_prec)
-         << setw(width) << setup_cpu_time + mult_cpu_time << " ms: "
-         << setw(width) << setup_cpu_time << " (setup) + "
-         << setw(width) << mult_cpu_time  << " (mult). "
+         << setw(width) << setup_cpu_time << " ms (setup) "
+         << setw(width) << prod_cpu_time  << " (*) "
          << setprecision(old_prec)
          << endl;
     cout.flags(old_flags);
+  }
+
+  static
+  int
+  enough_trials(double cpu_time)
+  {
+    const int min_trials_if_zero = 10000;
+    return (cpu_time == 0.0)
+        ? min_trials_if_zero 
+        : int(floor(MS_PER_S/cpu_time));
   }
 
   template< typename Multivector_T >
   static
   void
   time_mult(Multivector_T& a, const Multivector_T& b,
-            const index_set_t& inner_frame,
-            const index_set_t& outer_frame)
+            const typename Multivector_T::index_set_t& inner_frame,
+            const typename Multivector_T::index_set_t& outer_frame)
   {
-    const int min_trials_if_zero = 1000;
+    typedef Multivector_T multivector_t;
+    typedef typename multivector_t::index_set_t index_set_t;
+    typedef typename multivector_t::scalar_t scalar_t;
+
+    const scalar_t RAND_SCALE = 1.0/RAND_MAX;
+    const scalar_t rand1 = rand()*RAND_SCALE;
+
     Multivector_T c;
     clock_t cpu_time = clock();
-     c = a * b * (rand()*RAND_SCALE) + a * (rand()*RAND_SCALE);
+     c = a * (b * rand1 + scalar_t(1.0));
     double setup_cpu_time = elapsed(cpu_time);
     if (setup_cpu_time < MS_PER_S)
     {
-      const int max_trials =
-       setup_cpu_time == 0.0 ?
-         min_trials_if_zero :
-         int(floor(MS_PER_S/setup_cpu_time));
+      const int max_trials = enough_trials(setup_cpu_time);
       cpu_time = clock();
       int trials;
       for (trials = 0; trials != max_trials; ++trials)
-        c = a * b * (rand()*RAND_SCALE) + a * (rand()*RAND_SCALE);
+        c = a * (b * rand1 + scalar_t(1.0));
       setup_cpu_time = elapsed(cpu_time)/trials;
     }
     a = c;
     cpu_time = clock();
      c = a * a;
-    double mult_cpu_time = elapsed(cpu_time);
-    if (mult_cpu_time < MS_PER_S)
+    double prod_cpu_time = elapsed(cpu_time);
+    if (prod_cpu_time < MS_PER_S)
     {
-      const int max_trials =
-       mult_cpu_time == 0.0 ?
-         min_trials_if_zero :
-         int(floor(MS_PER_S/mult_cpu_time));
+      const int max_trials = enough_trials(prod_cpu_time);
       cpu_time = clock();
       int trials;
       for (trials = 0; trials != max_trials; ++trials)
         c = a * a;
-      mult_cpu_time = elapsed(cpu_time)/trials;
+      prod_cpu_time = elapsed(cpu_time)/trials;
     }
-    print_times(inner_frame, outer_frame, setup_cpu_time, mult_cpu_time);
+    print_times(inner_frame, outer_frame, setup_cpu_time, prod_cpu_time);
   }
 
   template< class Multivector_T >
@@ -127,27 +136,26 @@ namespace glucat_mult_test
   void
   mult_test(const index_t n, const index_t max_n)
   {
-    cout << "Clifford algebra squaring test:" << endl;
-
     typedef Multivector_T multivector_t;
-    typedef index_set_t e_;
+    typedef typename multivector_t::index_set_t e_;
     static const index_t v_lo = e_::v_lo;
     static const index_t v_hi = e_::v_hi;
+    static const index_t index_lim = std::min(-v_lo, v_hi);
+    if (n > index_lim)
+    {
+      cout << "Value " << n << " is too big." << endl;
+      cout << "Maximum value possible is " << index_lim << "." << endl;
+      return;
+    }
+    cout << "Clifford algebra squaring test:" << endl;
     const index_t max_index = min(n, max_n);
-    const e_ outer_frame = index_range<v_lo,v_hi>(-max_index, max_index);
+    const e_ outer_frame = e_(make_pair(-max_index, max_index));
 
-    clock_t cpu_time = clock();
     multivector_t a = multivector_t(outer_frame, 1);
-    const double setup_cpu_time = elapsed(cpu_time);
 
-    cout << "Square of unit pseudoscalar (" << a << ")" << endl;
+    cout << a << " * " << a << " == " << (a * a) << endl;
+    cout << endl;
 
-    cpu_time = clock();
-     a *= a;
-    const double mult_cpu_time = elapsed(cpu_time);
-
-    print_times(outer_frame, outer_frame, setup_cpu_time, mult_cpu_time);
-    cout << "Square is " << a << endl;
     e_ inner_frame;
     a = 1;
     inner_frame = e_();
