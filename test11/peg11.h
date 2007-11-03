c7ed75b207e2e85a3cb86f8f4eb80de97d661f08
Paul C. Leopardi <leopardi@users.sourceforge.net>
2007-11-03 23:07:48 +0000

    Make test11 into a regression test which uses the new function check() to compare results. Use many more test cases with function transcendtest().

--- test11/peg11.h
+++ test11/peg11.h
@@ -33,8 +33,38 @@
 
 namespace peg11
 {
-  using namespace glucat;
   using namespace std;
+  using namespace glucat;
+
+  template< class Multivector_T >
+  static
+  void 
+  check(const Multivector_T& A, const Multivector_T& B, const std::string& msg, const bool need_inv = false)
+  {
+    typedef Multivector_T m_;
+    typedef typename m_::scalar_t s_;
+    const s_ tol = s_(numeric_limits<s_>::epsilon()) * s_(1 << 7);
+    const s_ tol2 = tol*tol;
+    const s_ norm_diff = (norm(A) < tol2 || norm(B) < tol2) ? norm(A-B) : norm(A-B)/norm(A);
+    const bool A_isnan = A.isnan();
+    const bool B_isnan = B.isnan();
+    if ((A_isnan && !B_isnan) 
+     ||(!A_isnan && !need_inv && B_isnan) 
+     ||(!A_isnan && need_inv && !inv(A).isnan() && B_isnan) 
+     || norm_diff > tol2)
+    {
+      std::cout << "Test failed: ";
+      B.write(msg);
+      if (norm_diff > tol2)
+      {
+        if (norm(A) < tol2 || norm(B) < tol2)
+          std::cout << "Norm";
+        else
+          std::cout << "Relative norm";
+        std::cout << " of difference == " << sqrt(norm_diff) << std::endl;
+      }
+    } 
+  }
 
   template< class Multivector_T >
   static
@@ -45,24 +75,24 @@ namespace peg11
     typedef typename m_::scalar_t s_;
     const streamsize prec = cout.precision(numeric_limits<s_>::digits10);
     A.write("A");
-    sqrt(A).write("sqrt(A)");
-    exp(A).write("exp(A)");
-    cos(A).write("cos(A)");
-    cosh(A).write("cosh(A)");
-    sin(A).write("sin(A)");
-    sinh(A).write("sinh(A)");
-    tan(A).write("tan(A)");
-    tanh(A).write("tanh(A)");
-    cout << endl;
+    check(m_(1), exp(A)*exp(-A),    "exp(A)*exp(-A) != 1");
+    check(m_(1), exp(-A)*exp(A),    "exp(-A)*exp(A) != 1");
+    check(exp(A), std::exp(scalar(A))*exp(pure(A)),
+                                    "std::exp(scalar(A))*exp(pure(A)) != exp(A)");
+    check(exp(elliptic(A)*A), cos(A)+elliptic(A)*sin(A), 
+                                    "cos(A)+elliptic(A)*sin(A) != exp(elliptic(A)*A)");
+    check(exp(A), cosh(A)+sinh(A),  "cosh(A)+sinh(A) != exp(A)");
+    check(sin(A), cos(A)*tan(A),    "cos(A)*tan(A) != sin(A)");
+    check(sinh(A), cosh(A)*tanh(A), "cosh(A)*tanh(A) != sinh(A)");
 
-    (sqrt(A)*sqrt(A)).write("sqrt(A)*sqrt(A)");
-    exp(log(A)).write("exp(log(A))");
-    cos(acos(A)).write("cos(acos(A))");
-    cosh(acosh(A)).write("cosh(acosh(A))");
-    sin(asin(A)).write("sin(asin(A))");
-    sinh(asinh(A)).write("sinh(asinh(A))");
-    tan(atan(A)).write("tan(atan(A))");
-    tanh(atanh(A)).write("tanh(atanh(A))");
+    check(A, sqrt(A)*sqrt(A), "sqrt(A)*sqrt(A) != A");
+    check(A, exp(log(A)),     "exp(log(A)) != A", true);
+    check(A, cos(acos(A)),    "cos(acos(A)) != A", true);
+    check(A, cosh(acosh(A)),  "cosh(acosh(A)) != A", true);
+    check(A, sin(asin(A)),    "sin(asin(A)) != A", true);
+    check(A, sinh(asinh(A)),  "sinh(asinh(A)) != A", true);
+    check(A, tan(atan(A)),    "tan(atan(A)) != A", true);
+    check(A, tanh(atanh(A)),  "tanh(atanh(A)) != A", true);
     cout << endl;
     cout.precision(prec);
   }
@@ -78,14 +108,32 @@ namespace peg11
     const m_ i("{-1}");
     transcendtest(m_(0));
     transcendtest(m_(1));
+    transcendtest(-m_(1));
     transcendtest(m_(2));
+    transcendtest(-m_(2));
     transcendtest(m_(s_(0.5)));
+    transcendtest(-m_(s_(0.5)));
     transcendtest(m_(pi));
     // exp(i*pi) should be -1
+    transcendtest(pi*m_("{1,2,3}"));
+    transcendtest(-pi*m_("{1,2,3}"));
+    transcendtest(pi*m_("{-1,1,2,3,4}"));
+    transcendtest(-pi*m_("{-1,1,2,3,4}"));
+    transcendtest(pi*m_("{-2,-1,1,2,3,4,5}"));
+    transcendtest(-pi*m_("{-2,-1,1,2,3,4,5}"));
+    transcendtest(pi*m_("{-2,-1,1,2,3,4,5}")*s_(10.0));
+    transcendtest(-pi*m_("{-2,-1,1,2,3,4,5}")*s_(10.0));
     transcendtest(pi*m_("{1,2}"));
     transcendtest(pi*i/s_(2.0));
+    transcendtest(-pi*i/s_(2.0));
+    transcendtest(pi*i*s_(100.0));
     transcendtest(-pi*i*s_(100.0));
     transcendtest(m_("{1}"));
+    transcendtest(-m_("{1}"));
+    transcendtest(m_("{-1,1}"));
+    transcendtest(-m_("{-1,1}"));
+    transcendtest(m_("{-2,-1,1,2}"));
+    transcendtest(-m_("{-2,-1,1,2}"));
     transcendtest(m_("{-1}+{1}"));
     // the following should produce NaN values
     transcendtest(m_("{-1}+{1}")*s_(numeric_limits<s_>::max()));
