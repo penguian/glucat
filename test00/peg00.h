1cf02112ab20f155af9fefd615ac12c1636a722e
Paul C. Leopardi <leopardi@users.sourceforge.net>
2010-05-27 15:00:50 +0000

    Change print_a_b_lhs_rhs() to print_error_lhs_rhs(), delete print_a_b_c_lhs_rhs(): o The new routine print_error_lhs_rhs() prints only the relative error (assuming   rhs is not 0) and the error tolerance, if this is not 0. The previous routines   produced output which was too verbose.
    In test_idents():
    o Change last parameter from multivector_t e to index_set_t e: the frame
      containing a, b and c.
    o Set tolerance tol to either 0 (if test_control.m_verbose_output is true)
      or otherwise to 512.0*std::numeric_limits<scalar_t>::epsilon().
    o Change most tests to call print_error_lhs_rhs() if abs(lhs - rhs) > abs(rhs)*tol.
      When test_control.m_verbose_output is true, this calls print_error_lhs_rhs()
      whenever lhs != rhs.
    o Those tests which create scalar expressions call print_error_lhs_rhs() if
      scalar_diff*scalar_diff > tol*tol. When test_control.m_verbose_output is true,
      this calls print_error_lhs_rhs() whenever scalar_diff != 0.
    
    In do_test00():
    o Use new static multivector_t::random() function to give random values to
      multivector_t variables a, b and c, instead of calling rand().
    o Call test_idents() with paramaters a, b, c, and e: the frame containing
      a, b and c.

--- test00/peg00.h
+++ test00/peg00.h
@@ -51,70 +51,37 @@ namespace peg00
 
   using namespace glucat;
 
-  const double RAND_SCALE = 1.0/RAND_MAX;
-
   template< typename Multivector_T >
   static
   void
-  print_a_b_lhs_rhs(const Multivector_T& a, const Multivector_T& b,
-                    const Multivector_T& lhs, const Multivector_T& rhs) 
+  print_error_lhs_rhs(const Multivector_T& lhs, const Multivector_T& rhs, typename Multivector_T::scalar_t tol)
   {
     std::cout
-      << "  a == " << a
-      << std::endl
-      << "  b == " << b
-      << std::endl
-      << "  lhs == " << lhs
-      << std::endl
-      << "  rhs == " << rhs
-      << std::endl
-      << "  lhs-rhs == " << lhs - rhs
-      << std::endl;
-  }
-
-  template< typename Multivector_T >
-  static
-  void
-  print_a_b_c_lhs_rhs(const Multivector_T& a, const Multivector_T& b, const Multivector_T& c,
-                    const Multivector_T& lhs, const Multivector_T& rhs) 
-  {
-    std::cout
-      << "  a == " << a
-      << std::endl
-      << "  b == " << b
-      << std::endl
-      << "  c == " << c
-      << std::endl
-      << "  lhs == " << lhs
-      << std::endl
-      << "  rhs == " << rhs
-      << std::endl
-      << "  lhs-rhs == " << lhs - rhs
-      << std::endl;
+        << "  Relative norm of difference == " << abs(lhs - rhs)/abs(rhs) << std::endl;
+    if (tol > 0.0)
+      std::cout
+        << "  Tolerance                   == " << tol << std::endl;
   }
 
   template< typename Multivector_T >
   static
   bool
   test_idents(Multivector_T& a, Multivector_T& b, 
-              Multivector_T& c, const Multivector_T& e)
+              Multivector_T& c, typename Multivector_T::index_set_t e)
   {
     typedef Multivector_T multivector_t;
     typedef typename multivector_t::scalar_t scalar_t;
+    typedef typename multivector_t::index_set_t index_set_t;
 
-    const scalar_t random_a = rand()*RAND_SCALE;
-    const scalar_t random_b = rand()*RAND_SCALE;
-    const scalar_t random_c = rand()*RAND_SCALE;
-    const scalar_t tol = std::numeric_limits<scalar_t>::epsilon() * scalar_t(2);
+    static const scalar_t eps = std::numeric_limits<scalar_t>::epsilon();
+    const scalar_t tol = (test_control.m_verbose_output)
+      ? 0.0
+      : eps * 512.0;
 
     multivector_t lhs;
     multivector_t rhs;
 
     bool success = true;
-
-    a *= (e * random_a + scalar_t(1.0));
-    b *= (e * random_b + scalar_t(1.0));
-    c *= (e * random_c + scalar_t(1.0));
 
     const index_t a_grade = a.frame().count();
     const index_t b_grade = b.frame().count();
@@ -132,14 +99,14 @@ namespace peg00
           const multivector_t b_s = b(s);
           lhs = a_r & b_s;
           rhs = (a_r * b_s)(index_t(std::abs(r-s)));
-    
-          if ((norm(lhs - rhs) > tol))
+
+          if ((abs(lhs - rhs) > abs(rhs)*tol))
           {
             std::cout << "Identity [HS] (1.21a) failed in " << e
                       << ": r == " << r 
                       << ", s == " << s 
                       << std::endl;
-            print_a_b_lhs_rhs(a, b, lhs, rhs);
+            print_error_lhs_rhs(lhs, rhs, tol);
             success = false;
           }
         }
@@ -153,16 +120,13 @@ namespace peg00
       {
         lhs = a(0) & b(r);
         rhs = a(r) & b(0);
-  
-        if ((norm(lhs) > tol) || (norm(rhs) > tol))
+
+        if ((abs(lhs) > tol) || (abs(rhs) > tol))
         {
           std::cout << "Identity [HS] (1.21b) failed in " << e
-                    << ": grade == " << r 
-                    << std::endl
-                    << " a(0) * b(r) == " << lhs
-                    << std::endl
-                    << " a(r) * b(0) == " << rhs
+                    << ": grade == " << r
                     << std::endl;
+          print_error_lhs_rhs(lhs, rhs, tol);
           success = false;
         }
       }
@@ -181,14 +145,14 @@ namespace peg00
           const multivector_t b_s = b(s);
           lhs = a_r ^ b_s;
           rhs = (a_r * b_s)(r+s);
-    
-          if ((norm(lhs - rhs) > tol))
+
+          if ((abs(lhs - rhs) > abs(rhs)*tol))
           {
             std::cout << "Identity [HS] (1.22a) failed in " << e
                       << ": r == " << r 
                       << ", s == " << s 
                       << std::endl;
-            print_a_b_lhs_rhs(a, b, lhs, rhs);
+            print_error_lhs_rhs(lhs, rhs, tol);
             success = false;
           }
         }
@@ -199,11 +163,11 @@ namespace peg00
       lhs = (a ^ b) ^ c;
       rhs = a ^ (b ^ c);
   
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [HS] (1.25a) failed in " << e
                   << std::endl;
-        print_a_b_c_lhs_rhs(a, b, c, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -213,11 +177,11 @@ namespace peg00
       lhs = a_1 * b;
       rhs = (a_1 & b) + (a_1 ^ b);
 
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [HS] (1.31) failed in " << e 
                   << std::endl;
-            print_a_b_lhs_rhs(a, b, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -227,11 +191,11 @@ namespace peg00
       lhs = a_1 * b;
       rhs = (a_1 % b) + (a_1 ^ b);
 
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [D01] (Section 2.3 Example 2, vector) failed in " << e 
                   << std::endl;
-            print_a_b_lhs_rhs(a, b, lhs, rhs);
+            print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -242,11 +206,11 @@ namespace peg00
       lhs =  a_2 * b_m_b_1;
       rhs = (a_2 & b_m_b_1) + (a_2 * b_m_b_1 - b_m_b_1 * a_2)/scalar_t(2) + (a_2 ^ b_m_b_1);
 
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [HS] (1.63) failed in " << e 
                   << std::endl;
-            print_a_b_lhs_rhs(a, b, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -256,11 +220,11 @@ namespace peg00
       lhs =  a_2 * b;
       rhs = (a_2 % b) + (a_2 * b - b * a_2)/scalar_t(2) + (a_2 ^ b);
 
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [D01] (Section 2.3 Example 2, bivector) failed in " << e 
                   << std::endl;
-            print_a_b_lhs_rhs(a, b, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -270,11 +234,11 @@ namespace peg00
       const scalar_t scalar_rhs = scalar(a * b);
   
       const scalar_t scalar_diff = scalar_lhs - scalar_rhs;
-      if (scalar_diff*scalar_diff > tol)
+      if (scalar_diff*scalar_diff > tol*tol)
       {
         std::cout << "Identity [HS] (1.44) failed in " << e
                   << std::endl;
-        print_a_b_lhs_rhs(a, b, multivector_t(scalar_lhs), multivector_t(scalar_rhs));
+        print_error_lhs_rhs(multivector_t(scalar_lhs), multivector_t(scalar_rhs), tol);
         success = false;
       }
     }
@@ -284,11 +248,11 @@ namespace peg00
       const scalar_t scalar_rhs = star(reverse(a), reverse(b));
 
       const scalar_t scalar_diff = scalar_lhs - scalar_rhs;
-      if (scalar_diff*scalar_diff > tol)
+      if (scalar_diff*scalar_diff > tol*tol)
       {
         std::cout << "Identity [HS] (1.48) failed in " << e
                   << std::endl;
-        print_a_b_lhs_rhs(a, b, multivector_t(scalar_lhs), multivector_t(scalar_rhs));
+        print_error_lhs_rhs(multivector_t(scalar_lhs), multivector_t(scalar_rhs), tol);
         success = false;
       }
     }
@@ -298,11 +262,11 @@ namespace peg00
       lhs = a_0 % b;
       rhs = a_0 * b;
   
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [D01] (2.5) (a.2) failed in " << e
                   << std::endl;
-        print_a_b_lhs_rhs(a, b, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -311,13 +275,13 @@ namespace peg00
       const multivector_t a_1 = a(1);
       const multivector_t b_0 = b(0);
       lhs = a_1 % b_0;
-      rhs = scalar_t(0);
+      rhs = 0;
   
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [D01] (2.5) (a.3) failed in " << e
                   << std::endl;
-        print_a_b_lhs_rhs(a, b, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -327,11 +291,11 @@ namespace peg00
       lhs = a_1 % (b ^ c);
       rhs = ((a_1 % b) ^ c) + (involute(b) ^ (a_1 % c));
   
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [D01] (2.5) (c) failed in " << e
                   << std::endl;
-        print_a_b_c_lhs_rhs(a, b, c, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -340,11 +304,11 @@ namespace peg00
       lhs = (a ^ b) % c;
       rhs = a % (b % c);
   
-      if (norm(lhs - rhs) > tol)
+      if ((abs(lhs - rhs) > abs(rhs)*tol))
       {
         std::cout << "Identity [D01] (2.5) (d) failed in " << e
                   << std::endl;
-        print_a_b_c_lhs_rhs(a, b, c, lhs, rhs);
+        print_error_lhs_rhs(lhs, rhs, tol);
         success = false;
       }
     }
@@ -360,23 +324,21 @@ namespace peg00
     typedef typename multivector_t::index_set_t index_set_t;
     typedef typename multivector_t::scalar_t scalar_t;
 
-    const scalar_t random_a = rand()*RAND_SCALE;
-    const scalar_t random_b = rand()*RAND_SCALE;
-    const scalar_t random_c = rand()*RAND_SCALE;
-
-    multivector_t a = random_a;
-    multivector_t b = random_b;
-    multivector_t c = random_c;
-
     bool success = true;
 
-    index_set_t inner_frame = index_set_t();
+    index_set_t frm = index_set_t();
     for (index_t i = 1; i != max_index+1; i++)
     {
-      inner_frame |= index_set_t(i);
-      success &= test_idents(a, b, c, multivector_t(index_set_t(i) , scalar_t(1)));
-      inner_frame |= index_set_t(-i);
-      success &= test_idents(a, b, c, multivector_t(index_set_t(-i), scalar_t(1)));
+      frm |= index_set_t(i);
+      multivector_t a = multivector_t::random(frm);
+      multivector_t b = multivector_t::random(frm);
+      multivector_t c = multivector_t::random(frm);
+      success &= test_idents(a, b, c, frm);
+      frm |= index_set_t(-i);
+      a = multivector_t::random(frm);
+      b = multivector_t::random(frm);
+      c = multivector_t::random(frm);
+      success &= test_idents(a, b, c, frm);
     }
     if (success)
       std::cout << "All tests passed." << std::endl;
