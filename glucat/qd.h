60f86c179fa0068c4201abe24ed36cca88ff3cdd
Paul C. Leopardi <leopardi@users.sourceforge.net>
2010-05-27 14:58:19 +0000

    Separate the definitions for dd_real and qd_real, rather than interleaving them.
    Define macro _GLUCAT_QD_F(_T, _F) to reduce duplication in this file.
    Here _T is a type and _F is a function.
    
    For numeric_traits<dd_real> and numeric_traits<qd_real>:
    o Use _GLUCAT_QD_F() to define new functions cos(), acos(), cosh(), sin(),
      asin(), sinh(), tan(), atan(), tanh().
    o Also use _GLUCAT_QD_F() to define log() and exp() functions.
    o Define new function to_double().

--- glucat/qd.h
+++ glucat/qd.h
@@ -37,20 +37,20 @@ namespace glucat
   /// Extra traits which extend numeric limits
   // Reference: [AA], 2.4, p. 30-31
 
+#define _GLUCAT_QD_F(_T, _F) \
+  template<>           \
+  inline               \
+  const _T             \
+  numeric_traits<_T>:: \
+  _F(const _T& val)    \
+  { return ::_F(val); }
+
   /// Smart isnan for dd_real
   template<>
   inline
   bool
   numeric_traits<dd_real>::
   isNaN(const dd_real& val)
-  { return val.isnan(); }
-
-  /// Smart isnan for qd_real
-  template<>
-  inline
-  bool
-  numeric_traits<qd_real>::
-  isNaN(const qd_real& val)
   { return val.isnan(); }
 
   /// Smart isinf for dd_real
@@ -61,28 +61,12 @@ namespace glucat
   isInf(const dd_real& val)
   { return val.isinf(); }
 
-  /// Smart isinf for qd_real
-  template<>
-  inline
-  bool
-  numeric_traits<qd_real>::
-  isInf(const qd_real& val)
-  { return val.isinf(); }
-
   /// Smart isnan or isinf for dd_real
   template<>
   inline
   bool
   numeric_traits<dd_real>::
   isNaN_or_isInf(const dd_real& val)
-  { return val.isnan() || val.isinf(); }
-
-  /// Smart isnan or isinf for qd_real
-  template<>
-  inline
-  bool
-  numeric_traits<qd_real>::
-  isNaN_or_isInf(const qd_real& val)
   { return val.isnan() || val.isinf(); }
 
   /// to_int for dd_real
@@ -93,13 +77,13 @@ namespace glucat
   to_int(const dd_real& val)
   { return ::to_int(val); }
 
-  /// to_int for qd_real
+  /// to_double for dd_real
   template<>
   inline
-  int
-  numeric_traits<qd_real>::
-  to_int(const qd_real& val)
-  { return ::to_int(val); }
+  double
+  numeric_traits<dd_real>::
+  to_double(const dd_real& val)
+  { return ::to_double(val); }
 
   /// Modulo function for dd_real
   template<>
@@ -109,21 +93,13 @@ namespace glucat
   fmod(const dd_real& lhs, const dd_real& rhs)
   { return ::fmod(lhs, rhs); }
 
-  /// Modulo function for qd_real
-  template<>
-  inline
-  const qd_real
-  numeric_traits<qd_real>::
-  fmod(const qd_real& lhs, const qd_real& rhs)
-  { return ::fmod(lhs, rhs); }
-
   /// pow for dd_real
   template<>
   inline
   const dd_real
   numeric_traits<dd_real>::
   pow(const dd_real& val, int n)
-  { 
+  {
     dd_real result = 1.0;
     dd_real pow2 = val;
     for (int k = n; k != 0; pow2 *= pow2, k /= 2)
@@ -132,13 +108,110 @@ namespace glucat
     return result;
   }
 
+  /// Pi for dd_real
+  template<>
+  inline
+  const dd_real
+  numeric_traits<dd_real>::
+  pi()
+  { return dd_real::_pi; }
+
+  /// log(2) for dd_real
+  template<>
+  inline
+  const dd_real
+  numeric_traits<dd_real>::
+  ln_2()
+  { return dd_real::_log2; }
+
+  /// Exp of dd_real
+  _GLUCAT_QD_F(dd_real, exp)
+
+  /// Log of dd_real
+  _GLUCAT_QD_F(dd_real, log)
+
+  /// Cosine of dd_real
+  _GLUCAT_QD_F(dd_real, cos)
+
+  /// Inverse cosine of dd_real
+  _GLUCAT_QD_F(dd_real, acos)
+
+  /// Hyperbolic cosine of dd_real
+  _GLUCAT_QD_F(dd_real, cosh)
+
+  /// Sine of dd_real
+  _GLUCAT_QD_F(dd_real, sin)
+
+  /// Inverse sine of dd_real
+  _GLUCAT_QD_F(dd_real, asin)
+
+  /// Hyperbolic sine of dd_real
+  _GLUCAT_QD_F(dd_real, sinh)
+
+  /// Tangent of dd_real
+  _GLUCAT_QD_F(dd_real, tan)
+
+  /// Inverse tangent of dd_real
+  _GLUCAT_QD_F(dd_real, atan)
+
+  /// Hyperbolic tangent of dd_real
+  _GLUCAT_QD_F(dd_real, tanh)
+
+  /// Smart isnan for qd_real
+  template<>
+  inline
+  bool
+  numeric_traits<qd_real>::
+  isNaN(const qd_real& val)
+  { return val.isnan(); }
+
+  /// Smart isinf for qd_real
+  template<>
+  inline
+  bool
+  numeric_traits<qd_real>::
+  isInf(const qd_real& val)
+  { return val.isinf(); }
+
+  /// Smart isnan or isinf for qd_real
+  template<>
+  inline
+  bool
+  numeric_traits<qd_real>::
+  isNaN_or_isInf(const qd_real& val)
+  { return val.isnan() || val.isinf(); }
+
+  /// to_int for qd_real
+  template<>
+  inline
+  int
+  numeric_traits<qd_real>::
+  to_int(const qd_real& val)
+  { return ::to_int(val); }
+
+  /// to_double for qd_real
+  template<>
+  inline
+  double
+  numeric_traits<qd_real>::
+  to_double(const qd_real& val)
+  { return ::to_double(val); }
+
+  /// Modulo function for qd_real
+  template<>
+  inline
+  const qd_real
+  numeric_traits<qd_real>::
+  fmod(const qd_real& lhs, const qd_real& rhs)
+  { return ::fmod(lhs, rhs); }
+
   /// pow for qd_real
   template<>
   inline
   const qd_real
   numeric_traits<qd_real>::
   pow(const qd_real& val, int n)
-  { 
+  {
     qd_real result = 1.0;
     qd_real pow2 = val;
     for (int k = n; k != 0; pow2 *= pow2, k /= 2)
@@ -147,70 +220,54 @@ namespace glucat
     return result;
   }
 
-  /// Pi for dd_real
-  template<> 
-  inline
-  const dd_real 
-  numeric_traits<dd_real>::
-  pi()
-  { return dd_real::_pi; }
-
   /// Pi for qd_real
-  template<> 
+  template<>
   inline
-  const qd_real 
+  const qd_real
   numeric_traits<qd_real>::
   pi()
   { return qd_real::_pi; }
 
-  /// log(2) for dd_real
-  template<> 
-  inline
-  const dd_real 
-  numeric_traits<dd_real>::
-  ln_2()
-  { return dd_real::_log2; }
-
   /// log(2) for qd_real
-  template<> 
+  template<>
   inline
-  const qd_real 
+  const qd_real
   numeric_traits<qd_real>::
   ln_2()
   { return qd_real::_log2; }
 
-  /// Log for dd_real
-  template<>
-  inline
-  const dd_real
-  numeric_traits<dd_real>::
-  log(const dd_real& x)
-  { return ::log(x); }
+  /// Exp of qd_real
+  _GLUCAT_QD_F(qd_real, exp)
 
-  /// Log for qd_real
-  template<>
-  inline
-  const qd_real
-  numeric_traits<qd_real>::
-  log(const qd_real& x)
-  { return ::log(x); }
+  /// Log of qd_real
+  _GLUCAT_QD_F(qd_real, log)
 
-  /// exp for dd_real
-  template<>
-  inline
-  const dd_real
-  numeric_traits<dd_real>::
-  exp(const dd_real& val)
-  { return ::exp(val); }
+  /// Cosine of qd_real
+  _GLUCAT_QD_F(qd_real, cos)
 
-  /// exp for qd_real
-  template<>
-  inline
-  const qd_real
-  numeric_traits<qd_real>::
-  exp(const qd_real& val)
-  { return ::exp(val); }
+  /// Inverse cosine of qd_real
+  _GLUCAT_QD_F(qd_real, acos)
 
+  /// Hyperbolic cosine of qd_real
+  _GLUCAT_QD_F(qd_real, cosh)
+
+  /// Sine of qd_real
+  _GLUCAT_QD_F(qd_real, sin)
+
+  /// Inverse sine of qd_real
+  _GLUCAT_QD_F(qd_real, asin)
+
+  /// Hyperbolic sine of qd_real
+  _GLUCAT_QD_F(qd_real, sinh)
+
+  /// Tangent of qd_real
+  _GLUCAT_QD_F(qd_real, tan)
+
+  /// Inverse tangent of qd_real
+  _GLUCAT_QD_F(qd_real, atan)
+
+  /// Hyperbolic tangent of qd_real
+  _GLUCAT_QD_F(qd_real, tanh)
 }
 #endif
 
