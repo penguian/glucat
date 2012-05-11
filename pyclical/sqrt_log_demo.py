72edd954ca8bad31324e0321fe4c6634b0f6df8d
Paul C. Leopardi <leopardi@users.sourceforge.net>
2012-05-11 00:20:59 +0000

    Add comment block header. Do not define pq here - istpq is now defined in PyClical. In print statements where output is small, use abs().

--- pyclical/sqrt_log_demo.py
+++ pyclical/sqrt_log_demo.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
-import sys
-from PyCliCal import *
-pi = 3.14159265358979
+#
+# PyClical: Python interface to GluCat:
+#           Generic library of universal Clifford algebra templates
+#
+# sqrt_log_demo.py: Demonstrate various sqrt and log calculations with PyClical.
+#
+#    copyright            : (C) 2008-2012 by Paul C. Leopardi
+#
+#    This library is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyClical import *
 
 print '> e1=clifford("{1}")'
 e1         =clifford("{1}")
@@ -12,8 +31,8 @@ print '> print sqrt_e1'
 print          sqrt_e1
 print '> print sqrt_e1*sqrt_e1'
 print          sqrt_e1*sqrt_e1
-print '> print sqrt_e1*sqrt_e1-e1'
-print          sqrt_e1*sqrt_e1-e1
+print '> print abs(sqrt_e1*sqrt_e1-e1)'
+print          abs(sqrt_e1*sqrt_e1-e1)
 
 print '> log_e1=log(e1)'
 log_e1         =log(e1)
@@ -21,8 +40,8 @@ print '> print log_e1'
 print          log_e1
 print '> print exp(log_e1)'
 print          exp(log_e1)
-print '> print exp(log_e1)-e1'
-print          exp(log_e1)-e1
+print '> print abs(exp(log_e1)-e1)'
+print          abs(exp(log_e1)-e1)
 
 print '> v=clifford("-2{1}+2{2}-3{3}")'
 v         =clifford("-2{1}+2{2}-3{3}")
@@ -33,8 +52,8 @@ print '> print sqrt_v'
 print          sqrt_v
 print '> print sqrt_v*sqrt_v'
 print          sqrt_v*sqrt_v
-print '> print sqrt_v*sqrt_v-v'
-print          sqrt_v*sqrt_v-v
+print '> print abs(sqrt_v*sqrt_v-v)'
+print          abs(sqrt_v*sqrt_v-v)
 
 print '> log_v=log(v)'
 log_v         =log(v)
@@ -42,8 +61,8 @@ print '> print log_v'
 print          log_v
 print '> print exp(log_v)'
 print          exp(log_v)
-print '> print exp(log_v)-v'
-print          exp(log_v)-v
+print '> print abs(exp(log_v)-v)'
+print          abs(exp(log_v)-v)
 
 print '> x=clifford("-2{1}+2{2}-3{3}+4{-1,1,2}")'
 x         =clifford("-2{1}+2{2}-3{3}+4{-1,1,2}")
@@ -54,8 +73,8 @@ print '> print sqrt_x'
 print          sqrt_x
 print '> print sqrt_x*sqrt_x'
 print          sqrt_x*sqrt_x
-print '> print sqrt_x*sqrt_x-x'
-print          sqrt_x*sqrt_x-x
+print '> print abs(sqrt_x*sqrt_x-x)'
+print          abs(sqrt_x*sqrt_x-x)
 
 print '> log_x=log(x)'
 log_x         =log(x)
@@ -63,14 +82,11 @@ print '> print log_x'
 print          log_x
 print '> print exp(log_x)'
 print          exp(log_x)
-print '> print exp(log_x)-x'
-print          exp(log_x)-x
+print '> print abs(exp(log_x)-x)'
+print          abs(exp(log_x)-x)
 
-print "> pq=lambda p,q:index_set(str(range(-q,p+1)).replace('[','{').replace(']','}'))"
-pq=lambda p,q:index_set(str(range(-q,p+1)).replace('[','{').replace(']','}'))
-
-print '> x=random_clifford(pq(2,1))'
-x         =random_clifford(pq(2,1))
+print '> x=random_clifford(istpq(2,1))'
+x         =random_clifford(istpq(2,1))
 print '> print x'
 print          x
 
@@ -80,8 +96,8 @@ print '> print sqrt_x'
 print          sqrt_x
 print '> print sqrt_x*sqrt_x'
 print          sqrt_x*sqrt_x
-print '> print sqrt_x*sqrt_x-x'
-print          sqrt_x*sqrt_x-x
+print '> print abs(sqrt_x*sqrt_x-x)'
+print          abs(sqrt_x*sqrt_x-x)
 
 print '> log_x=log(x)'
 log_x         =log(x)
@@ -89,5 +105,5 @@ print '> print log_x'
 print          log_x
 print '> print exp(log_x)'
 print          exp(log_x)
-print '> print exp(log_x)-x'
-print          exp(log_x)-x
+print '> print abs(exp(log_x)-x)'
+print          abs(exp(log_x)-x)
