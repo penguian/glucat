580e0de91a27348260ccb38f8695bc2af1f6658c
Paul C. Leopardi <leopardi@users.sourceforge.net>
2012-05-16 02:17:43 +0000

    This script is now more usable as a demo. Many changes have been made, including:
    o New arguments:
    - azimuth : Angle about a vertical axis used to define the viewpoint.
    - jitter : Angle in degrees to use for each step in the rotation.
    
    o More sensible default values for arguments.
    
    o Do not use the twisted adjoint action |=, but instead calculate ir, the inverse of the rotor r once, and use the adjoint action u = r * u * ir. This works because r must be even, since it is th exponential of a bivector.
    
    o Do not plot lines. The code is now fast enough that this is no longer useful, since the hull of the scattered points can now more easily be seen.
    
    o Do not use plt.show(). This caused the plots to corrupt when the next figure was started.
    
    o Ensure the points lie within the arena by doubling the arena width and  recolouring existing points when necessary.

--- pyclical/plotting_demo.py
+++ pyclical/plotting_demo.py
@@ -22,7 +22,8 @@
 
 from PyClical import *
 
-def demo(d=4, nbr_curves=2, nbr_points=10000, scaling=200, segment_len=100, arena_width=10.0, rot_angle=30):
+def demo(d=4, nbr_curves=10, nbr_points=40000, scaling=2000, segment_len=4000,
+              arena_width=2.0, azimuth=210, rot_angle=90, jitter=10):
     """
     Plot curves created by exponentiating a random bivector in R_{d,0}.
     
@@ -33,7 +34,9 @@ def demo(d=4, nbr_curves=2, nbr_points=10000, scaling=200, segment_len=100, aren
     scaling     : Scaling constant to use with bivector.
     segment_len : Number of points in a curve segment.
     arena_width : Width of region used to determine colours for scatter plot.
+    azimuth     : Angle about a vertical axis used to define the viewpoint.
     rot_angle   : Angle in degrees to use to rotate each curve for display.
+    jitter      : Angle in degrees to use for each step in the rotation.
     """
     #
     # Constant to control the plotting.
@@ -42,7 +45,8 @@ def demo(d=4, nbr_curves=2, nbr_points=10000, scaling=200, segment_len=100, aren
     #
     # Imports needed for array calculation and plotting.
     #
-    from numpy import empty, array, mod
+    import numpy
+    from numpy import empty, amax, array, mod
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
     #
@@ -54,10 +58,9 @@ def demo(d=4, nbr_curves=2, nbr_points=10000, scaling=200, segment_len=100, aren
     # 
     rgb = empty((nbr_points, 3))
     #
-    # Plotting constants
+    # Plotting constant.
     #
-    azimuth = 30
-    half_arena_width = arena_width / 2.0
+    orig_arena_width = arena_width
     #
     # Plot nbr_curves curves.
     #
@@ -68,54 +71,76 @@ def demo(d=4, nbr_curves=2, nbr_points=10000, scaling=200, segment_len=100, aren
         fig = plt.figure(figsize = (15, 12))
         ax  =  fig.gca(projection = '3d')
         ax.view_init(azim=azimuth)
-        plt.show()
         #
-        # Initial point.
+        # Set the arena width back to the original value.
+        #
+        arena_width = orig_arena_width
+        #
+        # Initial point on the unit sphere.
         #
         u = cga3(random_clifford(istpq(3,0))(1))
+        u = u / abs(u)
         #
         # Use a random bivector in R_{d,0} with appropriate scaling.
         #
         b = random_clifford(istpq(d,0))(2) * scaling / nbr_points
         #
-        # Exponentiate the bivector to obtain a rotor.
+        # Exponentiate the bivector to obtain a rotor, and find its inverse.
         #
-        r = exp(b)
+        r  = exp(b)
+        ir = inv(r)
         #
         # Split the curve into M segments.
         #
         for j in xrange(M):
             #
             # Find segment_len points forming a curve segment
-            # by successively using the rotor r.
+            # by successively using the rotor r and its inverse..
             #
             abot = j*segment_len
             atop = abot + segment_len
             for k in xrange(abot, atop):
-                for h in xrange(0, 3):
-                    p[h, k] = agc3(u)[ist(h + 1)]
-                u |= r
+                p[:,k] = agc3(u).vector_part()
+                u = r * u * ir
             #
-            # Plot the curve segment.
+            # If the new points lie outside the arena, expand the arena.
+            #
+            if amax(numpy.abs(p[:, abot:atop])) * 2.0 > arena_width:
+                arena_width *= 2
+                if abot > 0:
+                    #
+                    # Re-colour the existing points.
+                    #
+                    for h in xrange(3):
+                        rgb[0:abot, h] = mod(p[h, 0:abot] + arena_width / 2.0,
+                                             arena_width) / arena_width
+                    #
+                    # Re-plot the existing points using the new colours.
+                    #
+                    ax.cla()
+                    X0 = p[0, 0:abot]
+                    Y0 = p[1, 0:abot]
+                    Z0 = p[2, 0:abot]
+                    ax.scatter(X0, Y0, Z0, c=rgb[0:abot, :], alpha=0.5, edgecolors='none')
+                    plt.draw()
+            #
+            # Colour the new scattered points.
+            #
+            for h in xrange(3):
+                rgb[abot:atop, h] = mod(p[h, abot:atop] + arena_width / 2.0,
+                                        arena_width) / arena_width
+            #
+            # Plot the new scattered points using the chosen colours.
             #
             alow = abot-1 if j > 0 else abot
             X = p[0, alow:atop]
             Y = p[1, alow:atop]
             Z = p[2, alow:atop]
-            ax.plot(X, Y, Z, c='gray', linestyle=':')
-            #
-            # Determine the colour of the scattered points.
-            #
-            for h in xrange(3):
-                rgb[abot:atop, h] = mod(p[h, abot:atop] + half_arena_width, arena_width) / arena_width
-            #
-            # Plot the scattered points using the chosen colour.
-            #
             ax.scatter(X, Y, Z, c=rgb[alow:atop, :], alpha=0.5, edgecolors='none')
             plt.draw()
         #
         # Rotate the plot about a vertical axis by rot_angle degrees
         #
-        for phi in xrange(rot_angle):
+        for phi in range(jitter, rot_angle + jitter, jitter):
             ax.view_init(azim=azimuth + phi)
             plt.draw()
