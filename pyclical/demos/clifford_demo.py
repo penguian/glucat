e0a819c8438f6ea803d7e14d438af0698330d6a9
Paul Leopardi <paul.leopardi@anu.edu.au>
2024-08-08 23:14:30 +1000

    Use raw strings for LaTeX to avoid 'SyntaxWarning: invalid escape sequence'

--- pyclical/demos/clifford_demo.py
+++ pyclical/demos/clifford_demo.py
@@ -55,15 +55,15 @@ def run(ctx):
     pause()
     print_fill("There is more than one way to multiply vectors.")
     print_line()
-    print_fill("The *scalar product* of $a$ and $b$ is denoted by $a \cdot b$ (or $(a,b)$ or $\langle a,b \\rangle$).")
-    print_fill("If we express $b$ as $b=b_{||}+b_{\perp}$, where $b_{||}$ is parallel to $a$, and"
-              +" $b_{\perp}$ is orthogonal to $a$, then we have")
-    print_fill("$|a \cdot b| = \| a \| \| b_{||} \|$, and $a \cdot b = b \cdot a$.")
+    print_fill(r"The *scalar product* of $a$ and $b$ is denoted by $a \cdot b$ (or $(a,b)$ or $\langle a,b \rangle$).")
+    print_fill(r"If we express $b$ as $b=b_{||}+b_{\perp}$, where $b_{||}$ is parallel to $a$, and"
+              +r" $b_{\perp}$ is orthogonal to $a$, then we have")
+    print_fill(r"$|a \cdot b| = \| a \| \| b_{||} \|$, and $a \cdot b = b \cdot a$.")
     print_exec("print(a & b); print(b & a)")
 
     pause()
     print_fill("Grassmann's *exterior (wedge) product* obeys different rules ...")
-    print_fill("$|a \wedge b| = \|a\| \|b_{\perp}\|$, and $a \wedge b = - b \wedge a$.")
+    print_fill(r"$|a \wedge b| = \|a\| \|b_{\perp}\|$, and $a \wedge b = - b \wedge a$.")
     print_exec("print(a ^ b); print(b ^ a)")
 
     pause()
@@ -71,17 +71,17 @@ def run(ctx):
     print_fill("Hermann Grassmann (1809-1877) [Wikimedia Commons]")
 
     pause()
-    print_fill("Given a basis $\\\{v_1, v_2, \ldots, v_n\\\} \subset \mathbb{R}^n$:")
-    print_fill("$v_1 \wedge v_2$ is a *bivector* - a directed area in a 2-plane.")
-    print_fill("$v_1 \wedge v_2 \wedge v_3$ is a *trivector* - a directed volume of a 3-space.")
-    print_fill("$v_1 \wedge v_2 \wedge \ldots \wedge v_n$ is an $n$-*vector* - a directed volume of an $n$-space.")
+    print_fill(r"Given a basis $\{v_1, v_2, \ldots, v_n\} \subset \mathbb{R}^n$:")
+    print_fill(r"$v_1 \wedge v_2$ is a *bivector* - a directed area in a 2-plane.")
+    print_fill(r"$v_1 \wedge v_2 \wedge v_3$ is a *trivector* - a directed volume of a 3-space.")
+    print_fill(r"$v_1 \wedge v_2 \wedge \ldots \wedge v_n$ is an $n$-*vector* - a directed volume of an $n$-space.")
     print_line()
-    print_fill("There is no $n+1$-vector in $n$-space. Why?")
+    print_fill(r"There is no $n+1$-vector in $n$-space. Why?")
 
     pause()
     print_fill("The wedge product detects linear dependence.")
-    print_fill("The set of vectors $\\\{v_1, v_2, \ldots, v_m\\\}$ is linearly dependent if and only if"
-              +" the product $v_1 \wedge v_2 \wedge \ldots \wedge v_m = 0$.")
+    print_fill(r"The set of vectors $\{v_1, v_2, \ldots, v_m\}$ is linearly dependent if and only if"
+              +r" the product $v_1 \wedge v_2 \wedge \ldots \wedge v_m = 0$.")
     print_exec("print(a)")
     print_exec("print(b)")
     print_exec("c = 3 * a - 2 * b; print(c)")
@@ -89,12 +89,12 @@ def run(ctx):
     print_exec("print(a ^ b ^ c)")
 
     pause()
-    print_fill("Given a basis $\\\{v_1, v_2, \ldots, v_n\\\} \subset \mathbb{R}^n$,"
-              +" the *Grassmann algebra* of $\mathbb{R}^n$ is the $2^n$-dimensional space of"
-              +" *multivectors* of the form")
-    print_fill("$x_{\\emptyset} + x_{\\\{1\\\}} v_1 + \ldots$"
-              +"$+ x_{\\\{n\\\}} v_n + x_{\\\{1,2\\\}} v_1 \wedge v_2 + \ldots + x_{\\\{n-1,n\\\}} v_{n-1} \wedge v_n + \ldots$"
-              +"$+ x_{\\\{1,\ldots,n\\\}} v_1 \wedge \ldots \wedge v_n$.")
+    print_fill(r"Given a basis $\{v_1, v_2, \ldots, v_n\} \subset \mathbb{R}^n$,"
+              +r" the *Grassmann algebra* of $\mathbb{R}^n$ is the $2^n$-dimensional space of"
+              +r" *multivectors* of the form")
+    print_fill(r"$x_{\\emptyset} + x_{\{1\}} v_1 + \ldots$"
+              +r"$+ x_{\{n\}} v_n + x_{\{1,2\}} v_1 \wedge v_2 + \ldots + x_{\{n-1,n\}} v_{n-1} \wedge v_n + \ldots$"
+              +r"$+ x_{\{1,\ldots,n\}} v_1 \wedge \ldots \wedge v_n$.")
     print_line()
     print_fill("The wedge product and Grassmann algebras are used in differential geometry and in physics and engineering.")
 
@@ -107,58 +107,58 @@ def run(ctx):
     print_fill("What if we *add* the inner product to the wedge product ... ?")
     print_exec("c = (a ^ b) + (a & b); print(c)")
     print_fill("This is called the *Clifford* (geometric) product of vectors.")
-    print_fill("$a b := a \wedge b + a \cdot b$.")
+    print_fill(r"$a b := a \wedge b + a \cdot b$.")
     print_exec("c = a * b; print(c)")
 
     pause()
     print_fill("The Clifford product has remarkable properties.")
-    print_fill("It detects when two vectors $v_1, v_2$ are orthogonal."
-              +" In this case, they *anticommute*: $v_1 v_2 = - v_2 v_1$.")
+    print_fill(r"It detects when two vectors $v_1, v_2$ are orthogonal."
+              +r" In this case, they *anticommute*: $v_1 v_2 = - v_2 v_1$.")
     print_exec("v_1 = e(1) + 2 * e(2); v_2 = e(2) - 2*e(1)")
     print_exec("print(v_1 * v_2); print(v_2 * v_1); print(v_1 & v_2)")
 
     pause()
     print_fill("The Clifford square of a vector is the square of its length.")
-    print_fill("$b b = b \wedge b + b \cdot b = b \cdot b = \|b\|^2$.")
+    print_fill(r"$b b = b \wedge b + b \cdot b = b \cdot b = \|b\|^2$.")
     print_exec("b = 3*e(1)+4*e(2); print(b); print(b*b); print(abs(b)**2)")
 
     pause()
-    print_fill("If $\\\{e_1, e_2, \ldots, e_n\\\}$ is an orthogonal basis of $\mathbb{R}^n$,"
+    print_fill(r"If $\{e_1, e_2, \ldots, e_n\}$ is an orthogonal basis of $\mathbb{R}^n$,"
               +" then the wedge and Clifford products of *disjoint* subsets of this basis coincide ...")
-    print_fill("$e_1 e_2 = e_1 \wedge e_2, \ldots, e_1 e_2 \ldots e_n = e_1 \wedge e_2 \wedge \ldots \wedge e_n$")
+    print_fill(r"$e_1 e_2 = e_1 \wedge e_2, \ldots, e_1 e_2 \ldots e_n = e_1 \wedge e_2 \wedge \ldots \wedge e_n$")
     print_exec("print(e(1)*e(2)); print(e(1) ^ e(2))")
-    print_fill("... so that every multivector $x$ in the $2^n$-dimensional Grassmann algebra on $\mathbb{R}^n$ can be expressed as")
-    print_fill("$x_{\\emptyset} + x_{\\\{1\\\}} e_1 + \ldots$"
-              +"$+ x_{\\\{n\\\}} e_n + x_{\\\{1,2\\\}} e_1 e_2 + \ldots + x_{\\\{n-1,n\\\}} e_{n-1} e_n + \ldots$"
-              +"$+ x_{\\\{1,\ldots,n\\\}} e_1 e_2 \ldots e_n$.")
-    print_fill("This $2^n$-dimensional vector space with the Clifford product is called"
-              +" the *Clifford algebra* on $\mathbb{R}^n$.")
+    print_fill(r"... so that every multivector $x$ in the $2^n$-dimensional Grassmann algebra on $\mathbb{R}^n$ can be expressed as")
+    print_fill(r"$x_{\\emptyset} + x_{\{1\}} e_1 + \ldots$"
+              +r"$+ x_{\{n\}} e_n + x_{\{1,2\}} e_1 e_2 + \ldots + x_{\{n-1,n\}} e_{n-1} e_n + \ldots$"
+              +r"$+ x_{\{1,\ldots,n\}} e_1 e_2 \ldots e_n$.")
+    print_fill(r"This $2^n$-dimensional vector space with the Clifford product is called"
+              +r" the *Clifford algebra* on $\mathbb{R}^n$.")
 
     pause()
     print_fill("So ... what about *relative directions in space*?")
-    print_fill("Well, now that we know that the Clifford square of every vector is the square of its length,"
+    print_fill(r"Well, now that we know that the Clifford square of every vector is the square of its length,"
               +" we know how to calculate the Clifford inverse of every non-zero vector: $a^{-1} = \\frac{a}{a a}$.")
     print_exec("print(a); print(inv(a)); print(inv(a)*a); print(a*inv(a))")
 
     pause()
-    print_fill("With the convention that $a/b := a b^{-1}$, we can now divide vectors.")
+    print_fill(r"With the convention that $a/b := a b^{-1}$, we can now divide vectors.")
     print_exec("print(a); print(a/b); print((a/b)*b)")
 
     pause()
     print_fill("Fine, but what use is that?")
     print_fill("It turns out that the ability to divide vectors is very useful in expressing orthogonal transformations.")
-    print_fill("The expression $-a b / a = -a b a^{-1}$ is the reflection of $b$ in the direction of $a$.")
-    print_fill("$-a b a^{-1} = -a (b_{\perp} + b_{||}) a^{-1} = -a b_{\perp} a^{-1} -a b_{||} a^{-1}$"
-              +"$= a a^{-1} b_{\perp} - a a^{-1} b_{||} = b_{\perp} - b_{||}$.")
+    print_fill(r"The expression $-a b / a = -a b a^{-1}$ is the reflection of $b$ in the direction of $a$.")
+    print_fill(r"$-a b a^{-1} = -a (b_{\perp} + b_{||}) a^{-1} = -a b_{\perp} a^{-1} -a b_{||} a^{-1}$"
+              +r"$= a a^{-1} b_{\perp} - a a^{-1} b_{||} = b_{\perp} - b_{||}$.")
     print_exec("print(a)")
     print_exec("print(b); print(abs(b))")
     print_exec("c = -a * b / a; print(c); print(abs(c))")
 
     pause()
-    print_fill("The *Cartan-Dieudonne' Theorem* says that every orthogonal transformation on $\mathbb{R}^n$"
-              +" can be expressed as the product of at most $n$ vectors.")
-    print_fill("In particular, $x \mapsto b a x (b a)^{-1}$ is a reflection in the direction of $a$"
-              +" followed by a reflection in the direction of $b$.")
+    print_fill(r"The *Cartan-Dieudonne' Theorem* says that every orthogonal transformation on $\mathbb{R}^n$"
+              +r" can be expressed as the product of at most $n$ vectors.")
+    print_fill(r"In particular, $x \mapsto b a x (b a)^{-1}$ is a reflection in the direction of $a$"
+              +r" followed by a reflection in the direction of $b$.")
     print_fill("This is also a rotation through *twice* the angle between $a$ and $b$.")
     print_exec("x = e(1)+2*e(2); print(x); print(x*x)")
     print_exec("y = (b * a) * x * inv(b * a); print(y); print(y*y)")
@@ -167,26 +167,26 @@ def run(ctx):
 
     pause()
     print_fill("You can take the exponential of a multivector, using the usual Taylor expansion ...")
-    print_fill("$\operatorname{e}^{x} = 1 + x + x^2/2 + x^3/(3!) + \ldots$")
+    print_fill(r"$\operatorname{e}^{x} = 1 + x + x^2/2 + x^3/(3!) + \ldots$")
     print_fill("The exponential of a vector is always a scalar plus a vector.")
     print_exec("print(a); print(exp(a))")
     print_exec("print(b); print(exp(b))")
 
     pause()
-    print_fill("It turns out that every special orthogonal transformation can be expressed as"
-              +" the exponential of a bivector (when $n \geq 2$) ...")
-    print_fill("$x \mapsto \operatorname{e}^{B} x \operatorname{e}^{-B} = (-\operatorname{e}^{B}) x (-\operatorname{e}^{-B})$.")
+    print_fill(r"It turns out that every special orthogonal transformation can be expressed as"
+              +r" the exponential of a bivector (when $n \geq 2$) ...")
+    print_fill(r"$x \mapsto \operatorname{e}^{B} x \operatorname{e}^{-B} = (-\operatorname{e}^{B}) x (-\operatorname{e}^{-B})$.")
     print_exec("print(x); print(x*x)")
     print_exec("y = exp(a ^ b) * x * exp(-a ^ b); print(y); print(y*y)")
-    print_fill("... and the set of exponentials of bivectors forms a group called the"
-              +" *Spin group* $\operatorname{Spin}(n)$ over $\mathbb{R}^n$.")
+    print_fill(r"... and the set of exponentials of bivectors forms a group called the"
+              +r" *Spin group* $\operatorname{Spin}(n)$ over $\mathbb{R}^n$.")
 
     pause()
     print_fill("But whatever happened to *time* ...?")
-    print_fill("Einstein and Minkowski showed that *space-time* can be described using"
-              +" the *Minkowski space* $\mathbb{R}^{3,1}$.")
-    print_fill("This is a space with a basis $\\\{e_{\\\{-1\\\}},e_{\\\{1\\\}},e_{\\\{2\\\}},e_{\\\{3\\\}}\\\}$,"
-              +" where $e_{\\\{-1\\\}}^2 = -1$.")
+    print_fill(r"Einstein and Minkowski showed that *space-time* can be described using"
+              +r" the *Minkowski space* $\mathbb{R}^{3,1}$.")
+    print_fill(r"This is a space with a basis $\{e_{\{-1\}},e_{\{1\}},e_{\{2\}},e_{\{3\}}\}$,"
+              +r" where $e_{\{-1\}}^2 = -1$.")
     print_fill("In this space, the special orthogonal transformations connected to the identity"
               +" are called the *restricted Lorentz transformations*.")
     print_fill("Just like rotations in space, these can also be expressed as the exponential of a bivector.")
