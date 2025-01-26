babf06e359ad3c7cc5f6c39b87860be3b165d24e
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-01-27 09:35:05 +1100

    Improve Bash scripts to set up a Mayvi environment for the plotting demos

--- pyclical/demos/conda-install-mayavi.sh
+++ pyclical/demos/conda-install-mayavi.sh
@@ -1,9 +1,16 @@
 # Run using source
 # Use conda and pip to install PyQt5 and Mayavi
 
+# Run from bash using source
+if [ "${BASH_SOURCE-}" = "$0" ]; then
+  echo "Please run this script from Bash as follows: \$ source $0" >&2
+  exit 1
+fi
 conda create -y -n mayavi
 conda activate mayavi
 conda install 'python=3.9' -y -c conda-forge
 pip install PyQt5
 conda install mayavi -y -c conda-forge
 conda uninstall mesalib -y
+conda install cython -y -c conda-forge
+conda install matplotlib -y -c conda-forge
--- pyclical/demos/mayavi-env.sh
+++ pyclical/demos/mayavi-env.sh
@@ -1,6 +1,10 @@
-# Run using source
 # Define environment variables for Mayavi plotting demos
 
+# Run from bash using source
+if [ "${BASH_SOURCE-}" = "$0" ]; then
+  echo "Please run this script from Bash as follows: \$ source $0" >&2
+  exit 1
+fi
 export PYTHONPATH="$(cd ..;pwd):$PYTHONPATH"
 export QT_API="pyqt5"
 export QT_QPA_PLATFORM="xcb"
