8f2f14420c0cef802781274b6e2d7bc1dcb0eaf8
Paul Leopardi <paul.leopardi@anu.edu.au>
2025-03-05 00:39:25 +1100

    Improve the Mayavi2 installation script

--- pyclical/demos/kubuntu-24-conda-install-mayavi.sh
+++ pyclical/demos/kubuntu-24-conda-install-mayavi.sh
@@ -5,15 +5,16 @@ if [ "${BASH_SOURCE-}" = "$0" ]; then
   echo "Please run this script from Bash as follows: \$ source $0" >&2
   exit 1
 fi
-conda create -y -n mayavi
+
+conda create -n mayavi -y
 conda activate mayavi
-conda install 'python=3.9' -y -c conda-forge
+conda install 'python=3.9' -c conda-forge -y
+conda install cython -c conda-forge -y
+conda install matplotlib -c conda-forge -y
+conda install qd -c conda-forge -y
+conda install jupyter -c conda-forge -y
 pip uninstall PyQt5
 pip install PyQt5
-conda install mayavi -y -c conda-forge
+conda install 'mayavi=4.8.2' -c conda-forge -y
+conda install 'python=3.9' -c conda-forge -y
 conda uninstall mesalib -y
-conda install cython -y -c conda-forge
-conda install matplotlib -y -c conda-forge
-conda install qd -y -c conda-forge
-conda uninstall mayavi -y
-conda install mayavi -y -c conda-forge
